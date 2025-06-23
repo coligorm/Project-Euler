5dc77a15ac017283fd3de7e86c4bf051208e96ca
coligorm <colinrhysgorman@gmail.com>
2025-06-23 20:03:15 +1000

    Solved Problem 5

--- 5.py
+++ 5.py
@@ -6,10 +6,9 @@ Problem 5
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-Ans:
+Ans: 232792560
 
 '''
-from collections import Counter
 
 # Finding the LCM of 1-20
 # List all prime factors of 1-20
@@ -47,34 +46,61 @@ def find_prime_factors(num):
     while i**2 <= num:
         if (new_num % i == 0) and (is_prime(i)):
             pf.append(i)
-            print(pf)
+            # print(pf)
             new_num /= i
         else:
             i += 1
             
     return pf
 
-def prime_factors_dict(keys):
-    # Create a dictionary of all prime factors for each given number
-    d = dict.fromkeys(keys,0)
-    for k in d:
-        d[k] = find_prime_factors(k)
+def prime_factors_list(nums):
+    # Create a list of all prime factors for each given number
+    pfs = []
+    for i in nums:
+        pfs.append(find_prime_factors(i))
         
-    print(d)
+    # print(f"Prime Factor list of {nums} = {pfs}")
+    return pfs
 
-def count_primes(d):
+def count_primes(l):
     occurrences = {}
-    for v in d.values():
-        for i in v:
-            v.count(i)
-
+    for x in l:
+        if x not in occurrences.keys():
+            occurrences[x] = 1
+        else:
+            occurrences[x] += 1
+                
+                
+    # print(f"Occurrences of primes = {occurrences}")
+    return occurrences
 
 def main():
-    pfd = prime_factors_dict(input)
-    count_primes(pfd)
-
+    prime_factors = prime_factors_list(input)
+    
+    # create a dictionary storing all the highest primes
+    highest_primes =  {x:0 for x in range(20) if is_prime(x)}
+    for primes in prime_factors:
+        
+        prime_dict = count_primes(primes)
+        for key in prime_dict:
+            
+            if prime_dict[key] > highest_primes[key]:
+                highest_primes[key] = prime_dict[key]      
+    # print(f"Highest Prime Count \n{highest_primes}")
+    
+    # Using all prime numbers found as often as each occurs most often
+    total = 1
+    for key in highest_primes:
+        if highest_primes[key] > 0:
+            result = pow(key, highest_primes[key])
+            # print(f"{total} x {result} = ")
+            total *= result
+        # print(total)
+        
+    print(f"LCM of {input} = {total}")
 
 if __name__ == "__main__":
-    input = [12,30]
+    test = [12,30] # LCM = 60
+    input = range(1,21)
     
     main()
