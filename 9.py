8e10b32db4e9d47209da5d3ad6d2b1a0258c6436
coligorm <colinrhysgorman@gmail.com>
2025-06-26 22:26:19 +1000

    Solved Problem 9

--- 9.py
+++ 9.py
@@ -9,6 +9,8 @@ For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
+
+Ans: 31875000
 '''
 # Pythagorean triplet can be written as:
 # a = 2mn
@@ -16,6 +18,8 @@ Find the product abc.
 # c = m^2 + n^2
 # Therefore, 2mn + m^2 - n^2 + m^2 + n^2= 1000
 # Thus, 2mn + 2m^2 = 1000
+
+# I want m = 20 and n =5
 
 def calc_a(m,n):
     return 2 * m * n
@@ -27,7 +31,7 @@ def calc_c(m,n):
     return pow(m,2) + pow(n,2)
 
 def create_triplet(a,b,c):
-    return list(a,b,c)
+    return list((a,b,c))
 
 def is_Pythagorean_triplet(l):
     if l == sorted(l) and (pow(l[0],2) + pow(l[1],2) == pow(l[2],2)):
@@ -44,17 +48,21 @@ def product(l):
 def main():
     m = 2
     n = 1
+    while True:
+        a = calc_a(m,n)
+        b = calc_b(m,n)
+        c = calc_c(m,n)
+        if is_Pythagorean_triplet(create_triplet(a, b, c)) and a + b + c == 1000:
+            break
+        else:
+
+            if m - n < 15:
+                m += 1
+            else:
+                n += 1
     
-    
-    print(is_Pythagorean_triplet(test))
-    # while True:
-    #     if is_Pythagorean_triplet(test) and sum(test) != input:
-    #         print(f"[a,b,c] = {test}. The product abc = {product(test)}")
-    #         break
-            
-            
-        
-    pass
+    print(a,b,c)
+    print(product(create_triplet(a,b,c)))
 
 if __name__ == "__main__":
     input = 1000
