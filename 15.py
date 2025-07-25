8db737c8989fd518d37110ad2b50752d52088edb
coligorm <colinrhysgorman@gmail.com>
2025-07-25 16:34:01 +1000

    Added Problem 15

---
+++ 15.py
@@ -0,0 +1,21 @@
+'''
+Lattice Paths
+
+Problem 15
+
+Starting in the top left corner of a 2 x 2 grid,
+and only being able to move to the right and down,
+there are exactly 6 routes to the bottom right corner.
+
+_ _  _    _
+   |  |_   |
+   |    |  |_  etc.
+
+
+How many such routes are there through a 20 x 20 grid?
+'''
+def main():
+    pass
+
+if __name__ == "__main__":    
+    main()
