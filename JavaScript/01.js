ee264546ae6d6a8a9f615e7dba4d92d2de2054ac
coligorm <colinrhysgorman@gmail.com>
2025-09-09 23:54:18 +1000

    Solved Problem 1 in JavaScript

--- JavaScript/01.js
+++ JavaScript/01.js
@@ -4,6 +4,27 @@ Multiples of 3 or 5
 Problem 1
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
 
-Find the sum of all the multiples of 3 or 5 beloq 1000.
+Find the sum of all the multiples of 3 or 5 below 1000.
 
+Ans: 233168
 */
+let test = 10;
+let input = 1000;
+
+function mul_of_3_or_5(params) {
+    if (params % 5 == 0 || params % 3 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+let count = 0
+for (let index = 1; index < input; index++) {
+    if (mul_of_3_or_5(index)) {
+        count += index;
+    }
+}
+
+console.log("The sum of all multiples of 3 and 5 below " + input + " is " + count);
---
+++ JavaScript/README.md
@@ -0,0 +1,3 @@
+As I am currently learning [JavaScript](https://github.com/coligorm/Full-Stack-Learning/tree/main/JavaScriptLearning), I decided to try some of the easier problems I've already solved in Python.
+
+I may skip some challenges in JS and return as my knowledge _grows_.
