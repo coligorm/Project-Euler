7bce61321657e5b6e11bc4c422db8bb56e8ec840
coligorm <colinrhysgorman@gmail.com>
2025-09-12 17:39:03 +1000

    Solved Problem 19

--- Python/19.py
+++ Python/19.py
@@ -13,4 +13,26 @@ Which has twenty-eight, rain or shine.
 And on leap years, twenty-nine.
 A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+Ans: 171
 '''
+
+from datetime import date, timedelta
+
+# Source: https://stackoverflow.com/a/1060330
+def daterange(start_date: date, end_date: date):
+    days = int((end_date - start_date).days)
+    for n in range(days):
+        yield start_date + timedelta(n)
+
+def main():
+    count = 0
+    start_date = date(1901, 1, 1)
+    end_date = date(2001, 1, 1) # Exclusive range
+    for single_date in daterange(start_date, end_date):
+        if single_date.weekday() == 6 and single_date.day == 1:
+            count += 1
+    print(count)
+
+if __name__ == "__main__":  
+    main()
