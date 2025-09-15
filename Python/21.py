268504b398bbb55eb0e316478fdd05957dffc08d
coligorm <colinrhysgorman@gmail.com>
2025-09-15 23:59:21 +1000

    Optimised Problem 21

--- Python/21.py
+++ Python/21.py
@@ -10,9 +10,11 @@ The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
 Evaluate the sum of all the amicable numbers under 10000.
 '''
+import time
+
 def proper_numbers(n):
     pn = set([1])
-    for i in range(2, n // 2 + 1):
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             pn.add(i)
             if i != n // i:
@@ -20,17 +22,30 @@ def proper_numbers(n):
     return sorted(pn)
 
 def amicable(x, y):
-    return sum(proper_numbers(x)) == y and sum(proper_numbers(y)) == x and x != y
+    return sum(proper_numbers(y)) == x and x != y
 
 def main():
-    # Use sets to avoid duplicates 
-    amicable_pairs = set()
-    for i in range(input):
-        pn_sum = sum(proper_numbers(i))
-        if amicable(i, pn_sum):
-            amicable_pairs.add(i)
-            amicable_pairs.add(pn_sum)
-    print(f"Total: {sum(amicable_pairs)}")
+    # loop_counter = 0
+    # avg_time = 0
+    # while loop_counter < 10:
+        start = time.time()
+        # Use sets to avoid duplicates 
+        amicable_pairs = set()
+        for i in range(input):
+            pn_sum = sum(proper_numbers(i))
+            if amicable(i, pn_sum):
+                amicable_pairs.add(i)
+                amicable_pairs.add(pn_sum)
+        print(f"Total: {sum(amicable_pairs)}")
+    
+        # end = time.time()
+        # avg_time += end - start
+        # loop_counter += 1
+    
+    # print(f"Average time = {avg_time / 10}")
+    
+    # Before optimisation: Avg 3.92 seconds
+    # After optimisation: Avg 0.09 seconds
     
 if __name__ == "__main__":
     test = 220
