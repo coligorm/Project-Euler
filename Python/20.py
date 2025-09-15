c6ca1ec90e52244e9a0d5a630f45d8534b38f361
coligorm <colinrhysgorman@gmail.com>
2025-09-15 20:08:35 +1000

    Solved Problem 20

--- Python/20.py
+++ Python/20.py
@@ -9,17 +9,19 @@ and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
 '''
-def factorial():
-    pass
+def factorial(n): # Recurive factorial
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-def num_to_list(l):
-    pass
-
+def num_to_list(n):
+    return list(map(int, str(n)))
 
 def main():
-    fact = factorial(test)
+    fact = factorial(input)
+    print(sum(num_to_list(fact)))
     
-
 if __name__ == "__main__":
     test = 10
     input = 100
