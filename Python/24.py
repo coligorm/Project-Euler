2777d7640d779dc7d9080b0df9e0eab073076958
coligorm <colinrhysgorman@gmail.com>
2025-09-20 17:16:46 +1000

    Solved Problem 24 [15m]

--- Python/24.py
+++ Python/24.py
@@ -9,5 +9,19 @@ The lexicographic permutations of 0, 1 and 2 are:
 012   021   102   120   201   210
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
-
+Ans: 2783915460
 '''
+from itertools import permutations  
+
+def main():
+    numbers = [int(x) for x in input]
+    perm = sorted(list(permutations(numbers, len(numbers))))
+    result = int(''.join(map(str, perm[((999_999))])))
+
+    print(result)
+
+if __name__ == '__main__':
+    test = '012'
+    input = '0123456789'
+
+    main()
